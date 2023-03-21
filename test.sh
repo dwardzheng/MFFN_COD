197a6c05260c7c06e8239f2f8bcc3c1b9e98f5dc
dwardzheng <54825849+dwardzheng@users.noreply.github.com>
2023-03-21 11:29:53 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,15 +8,15 @@ export CUDA_VISIBLE_DEVICES="$1"
 echo 'Excute the script on GPU: ' "$1"
 
 echo 'For COD'
-python test.py --config ./configs/zoomnet/cod_zoomnet.py \
-    --model-name ZoomNet \
+python test.py --config ./configs/MFFN/MFFN_R50.py \
+    --model-name MFFN \
     --batch-size 22 \
-    --load-from ./output/ForSharing/cod_zoomnet_r50_bs8_e40_2022-03-04.pth \
+    --load-from ./output/model_final.pth \
     --save-path ./output/ForSharing/COD_Results
 
 echo 'For SOD'
-python test.py --config ./configs/zoomnet/sod_zoomnet.py \
-    --model-name ZoomNet \
+python test.py --config ./configs/MFFN/MFFN_R50.py \
+    --model-name MFFN \
     --batch-size 22 \
-    --load-from ./output/ForSharing/sod_zoomnet_r50_bs22_e50_2022-03-04_fixed.pth \
+    --load-from ./output/model_final.pth \
     --save-path ./output/ForSharing/SOD_Results
